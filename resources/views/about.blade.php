84eac8c33c80887f7cc83638f3e7c4f157d7238a
asimon84 <ajsimon4@yahoo.com>
2025-05-28 15:30:08 -0700

    about page update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,13 @@
 <x-layouts.app :title="__('About')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <h3>
+                About
+            </h3>
+
+            <p>
+                This is a simple example of a REST API.
+            </p>
         </div>
     </div>
 </x-layouts.app>
