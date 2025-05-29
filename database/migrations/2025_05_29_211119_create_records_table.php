90995626b2e4864f3a8af7a64c03373dd7ab5d47
asimon84 <ajsimon4@yahoo.com>
2025-05-29 14:51:28 -0700

    initial record migration and seeder

--- database/migrations/2025_05_29_211119_create_records_table.php
+++ database/migrations/2025_05_29_211119_create_records_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('records', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
         });
     }
