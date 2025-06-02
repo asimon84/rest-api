5b531c5c3247f293eb434dedac4dccfe79dab69e
asimon84 <ajsimon4@yahoo.com>
2025-06-02 15:26:10 -0700

    schedule task to prune old API tokens

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
+//php artisan inspire
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+//Once daily, prune all tokens expired for over 24 hours
+Schedule::command('sanctum:prune-expired --hours=24')->daily();
