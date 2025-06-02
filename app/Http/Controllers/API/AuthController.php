27225263fe67ccf355828e1dd4bfaa31f0d2c565
asimon84 <ajsimon4@yahoo.com>
2025-06-02 12:20:13 -0700

    initial API AuthController added

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+
+class AuthController extends Controller
+{
+//    /**
+//     * @param Request $request
+//     * @return mixed
+//     */
+//    public function register(Request $request)
+//    {
+//        $validator = Validator::make($request->all(), [
+//            'name' => 'required|string|max:255',
+//            'email' => 'required|string|email|max:255|unique:users',
+//            'password' => 'required|string|min:8|confirmed',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return response()->json(['error' => $validator->errors()], 401);
+//        }
+//
+//        $user = User::create([
+//            'name' => $request->name,
+//            'email' => $request->email,
+//            'password' => Hash::make($request->password),
+//        ]);
+//
+//        $token = $user->createToken('plainTextToken', [])->plainTextToken;
+//
+//        return response()->json([
+//            'success' => true,
+//            'message' => 'User registered successfully.',
+//            'token' => $token,
+//            'user' => $user,
+//        ]);
+//    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('plainTextToken', [])->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful.',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function profile(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'user' => $request->user(),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful.',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,23 @@
 
 use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/token/create', function (Request $request) {
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
 
-//Route::middleware(['auth:sanctum'])->group(function () {
+    $user = Auth::user();
+    $token = $user->createToken('plainTextToken', []);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
 //    Route::get('/user', [DashboardController::class, 'index'])->name('user');
-//    Route::get('/user', function (Request $request) {
-//        var_dump($request);
-//    });
-//});
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
