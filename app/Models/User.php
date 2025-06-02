349c8ae5000fc1b4dd5ebfaf2fddcf908a56a741
asimon84 <ajsimon4@yahoo.com>
2025-06-01 22:09:56 -0700

    user  model update

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /**
+     * @use HasApiTokens
+     * @use HasFactory
+     * @use Notifiable
+     */
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Route::middleware(['auth:sanctum'])->group(function () {
+//    Route::get('/user', [DashboardController::class, 'index'])->name('user');
+//    Route::get('/user', function (Request $request) {
+//        var_dump($request);
+//    });
+//});
