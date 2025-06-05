ca88acba86c15b4107d7d65a6f9e7448cb119949
asimon84 <ajsimon4@yahoo.com>
2025-06-05 16:16:11 -0700

    unit test updates

---
+++ tests/Feature/API/AuthTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_register_user(): void
+    {
+        $data = [
+            'name' => 'Feature Test User',
+            'email' => 'test@feature.com',
+            'password' => Hash::make('test1234')
+        ];
+
+        $response = $this->post('/api/register', $data);
+
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['message']);
+        $this->assertNotEmpty($response['token']);
+        $this->assertNotEmpty($response['user']);
+    }
+
+    public function test_create_token(): void
+    {
+        $user = User::factory()->create();
+        $user->password  = Hash::make('test1234');
+        $user->save();
+
+        $data = [
+            'email' => $user->email,
+            'password' => 'test1234'
+        ];
+
+        $response = $this->post('/api/token/create', $data);
+
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['message']);
+        $this->assertNotEmpty($response['token']);
+        $this->assertNotEmpty($response['user']);
+    }
+
+    public function test_login(): void
+    {
+        $user = User::factory()->create();
+        $user->password  = Hash::make('test1234');
+        $user->save();
+
+        $data = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'test1234'
+        ];
+
+        $response = $this->post('/api/login', $data);
+
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['message']);
+        $this->assertNotEmpty($response['token']);
+        $this->assertNotEmpty($response['user']);
+    }
+
+    public function test_logout(): void
+    {
+        $user = User::factory()->create();
+        $user->password  = Hash::make('test1234');
+        $user->save();
+
+        $data = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'test1234'
+        ];
+
+        $this->post('/api/login', $data);
+
+        $response = $this->post('/api/logout', $data);
+
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['message']);
+    }
+}
--- tests/Feature/API/UserTest.php
+++ tests/Feature/API/UserTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\API;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
@@ -11,19 +12,21 @@ class UserTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_register_user(): void
+    public function test_get_user(): void
     {
+        $user = User::factory()->create();
+        $user->password  = Hash::make('test1234');
+        $user->save();
+
         $data = [
-            'name' => "Feature Test User",
-            'email' => "test@feature.com",
-            'password' => Hash::make('test1234')
+            'email' => $user->email,
+            'password' => 'test1234'
         ];
 
-        $response = $this->post('/api/register', $data);
+        $response = $this->post('/api/token/create', $data);
 
         $this->assertTrue($response['success']);
         $this->assertNotEmpty($response['message']);
-        $this->assertNotEmpty($response['token']);
         $this->assertNotEmpty($response['user']);
     }
 }
