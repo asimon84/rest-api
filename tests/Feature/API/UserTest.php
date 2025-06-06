5039d45c4cf95fb9ad8fa577c3da044d87a1f9e1
asimon84 <ajsimon4@yahoo.com>
2025-06-06 12:56:51 -0700

    unit test  updates

--- tests/Feature/API/UserTest.php
+++ tests/Feature/API/UserTest.php
@@ -14,9 +14,7 @@ class UserTest extends TestCase
 
     public function test_get_user(): void
     {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)
+        $response = $this->actingAs(User::factory()->create())
             ->get('/api/user');
 
         $this->assertTrue($response['success']);
@@ -26,47 +24,41 @@ class UserTest extends TestCase
 
     public function test_patch_user(): void
     {
-        $user = User::factory()->create();
-
         $data = [
             'name' => 'New User Name',
             'email' => 'new@email.com',
             'password' => 'newpass1234',
         ];
 
-        $response = $this->actingAs($user)
+        $response = $this->actingAs(User::factory()->create())
             ->patch('/api/user', $data);
 
         $this->assertTrue($response['success']);
         $this->assertNotEmpty($response['message']);
         $this->assertNotEmpty($response['user']);
-        $this->assertNotEquals($response['user']['name'], $user->name);
+        $this->assertNotEquals($response['user']['name'], $data['name']);
     }
 
     public function test_put_user(): void
     {
-        $user = User::factory()->create();
-
         $data = [
             'name' => 'New User Name',
             'email' => 'new@email.com',
             'password' => 'newpass1234',
         ];
 
-        $response = $this->actingAs($user)
+        $response = $this->actingAs(User::factory()->create())
             ->put('/api/user', $data);
 
         $this->assertTrue($response['success']);
         $this->assertNotEmpty($response['message']);
         $this->assertNotEmpty($response['user']);
-        $this->assertNotEquals($response['user']['name'], $user->name);
+        $this->assertEquals($response['user']['name'], $data['name']);
     }
 
     public function test_delete_user(): void
     {
-        $user = User::factory()->create();
-
-        $response = $this->actingAs($user)
+        $response = $this->actingAs(User::factory()->create())
             ->delete('/api/user');
 
         $this->assertTrue($response['success']);
