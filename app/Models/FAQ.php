22a148aa30a9ab6f72a67828cb4db7dd5289eb16
asimon84 <ajsimon4@yahoo.com>
2025-09-15 11:14:39 -0700

    FAQ modification

--- app/Models/FAQ.php
+++ app/Models/FAQ.php
@@ -16,8 +16,13 @@ class FAQ extends Model
             'open' => false
         ],
         [
+            'title' => 'What is an API?',
+            'content' => 'An API is an Application Programming Interface. It allows different software applications to communicate with each other. It enables the exchange of data, features, and functionality between applications, simplifying and accelerating software development.',
+            'open' => false
+        ],
+        [
             'title' => 'What is a REST API?',
-            'content' => 'An API is an Application Programming Interface. It allows different software applications to communicate with each other. It enables the exchange of data, features, and functionality between applications, simplifying and accelerating software development. A REST API is an API that uses RESTful principles. REST stands for Representational State Transfer. REST defines how the architecture of a large-scale distributed system, such as the Web, should behave. The REST architectural style emphasises uniform interfaces, independent deployment of components, the scalability of interactions between them, and creating a layered architecture to promote caching to reduce user-perceived latency, enforce security, and encapsulate legacy systems. REST has been employed throughout the software industry to create stateless, reliable web-based applications.',
+            'content' => 'A REST API is an API that uses RESTful principles. REST stands for Representational State Transfer. REST defines how the architecture of a large-scale distributed system, such as the Web, should behave. The REST architectural style emphasises uniform interfaces, independent deployment of components, the scalability of interactions between them, and creating a layered architecture to promote caching to reduce user-perceived latency, enforce security, and encapsulate legacy systems. REST has been employed throughout the software industry to create stateless, reliable web-based applications.',
             'open' => false
         ],
     ];
