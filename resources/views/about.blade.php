6b01c7d210029501148795315de01b2cd3439ef0
asimon84 <ajsimon4@yahoo.com>
2025-09-15 11:13:33 -0700

    About page update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,19 @@
 <x-layouts.app :title="__('About')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <h3>
-                About
-            </h3>
 
-            <p>
+            <div class="heading">
+                <h3>Frequently Asked Questions</h3>
+            </div>
+
+            <p class="text">
                 This is a simple example of a REST API, an Application Programming Interface that uses RESTful principles to provide stateless interaction with test data.
             </p>
+
+            <p class="text">
+
+            </p>
+
         </div>
     </div>
 </x-layouts.app>
