1885d3833b991ac69d5d6352d2e8f5e9b91d98e3
asimon84 <ajsimon4@yahoo.com>
2025-09-16 16:07:22 -0700

    initial chart  data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard',  []);
+        $chartData = [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+        ];
+
+//        return view('dashboard', compact('chartData'));
+        return view('dashboard', []);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,9 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                {{--<x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />--}}
+
+                <div id="my-chart"></div>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
@@ -16,3 +18,17 @@
         </div>
     </div>
 </x-layouts.app>
+
+<script>
+    var chartData = [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+    ];
+
+    var chart = c3.generate({
+        bindto: '#my-chart', // ID of the HTML element
+        data: {
+            columns: chartData
+        }
+    });
+</script>
