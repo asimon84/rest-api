5a76a7394d22c31dc827c85ef235ef4b0ff9cb7e
asimon84 <ajsimon4@yahoo.com>
2025-10-03 19:32:49 -0700

    chart updates

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -27,13 +27,13 @@ new DataTable('#myDataTable', {
     serverSide: true,
     ajax: window.route,
     columns: [
-        {data: 'id', name: 'id'},
-        {data: 'string', name: 'string', className: 'truncate-text'},
-        {data: 'text', name: 'text', className: 'truncate-text'},
-        {data: 'json', name: 'json', className: 'truncate-text'},
-        {data: 'boolean', name: 'boolean'},
-        {data: 'integer', name: 'integer'},
-        {data: 'float', name: 'float'},
+        {data: 'id', name: 'id', searchable: true},
+        {data: 'string', name: 'string', className: 'truncate-text', searchable: true},
+        {data: 'text', name: 'text', className: 'truncate-text', searchable: true},
+        {data: 'json', name: 'json', className: 'truncate-text', searchable: true},
+        {data: 'boolean', name: 'boolean', searchable: true},
+        {data: 'integer', name: 'integer', searchable: true},
+        {data: 'float', name: 'float', searchable: true},
         {data: 'action', name: 'action', orderable: false, searchable: false},
     ]
 });
