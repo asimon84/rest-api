e0e0d0af260ad6a2123a1780eb697e3ada95a8ee
asimon84 <ajsimon4@yahoo.com>
2025-10-05 14:43:47 -0700

    logout failure message typo fix

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -173,7 +173,7 @@ class AuthController extends Controller
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'success' => false,
-                'message' => 'Task failed. Login unsuccessful.',
+                'message' => 'Task failed. Logout unsuccessful.',
                 'error' => 'Unauthorized'
             ], 401);
         }
